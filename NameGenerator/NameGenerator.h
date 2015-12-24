21f17072948f1d9150a3184717b354a07f25f6ce
Jason Larsen <jason.larsen@gmail.com>
2015-12-24 11:47:21 -0700

    Initial Commit

---
+++ NameGenerator/NameGenerator.h
@@ -0,0 +1,19 @@
+//
+//  NameGenerator.h
+//  NameGenerator
+//
+//  Created by Jason Larsen on 12/24/15.
+//  Copyright © 2015 Jason Larsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NameGenerator.
+FOUNDATION_EXPORT double NameGeneratorVersionNumber;
+
+//! Project version string for NameGenerator.
+FOUNDATION_EXPORT const unsigned char NameGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NameGenerator/PublicHeader.h>
+
+
